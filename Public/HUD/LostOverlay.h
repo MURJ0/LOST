27a1e134d996984765477f6a54b9b16d477be159
MURJ0 <ttrilo@abv.bg>
2024-02-08 00:26:14 +0200

    <created LostOverlay class>

---
+++ Public/HUD/LostOverlay.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LostOverlay.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOSTV2_API ULostOverlay : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
